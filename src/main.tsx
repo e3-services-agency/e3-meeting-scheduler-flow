d61d85ea8f5ed0c6f881e0d6747d782ff7c10ff8
e3-services-agency <tools@e3-services.com>
2025-08-26 21:25:14 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/sonner";
-import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 
 import Landing from "./pages/Landing";
